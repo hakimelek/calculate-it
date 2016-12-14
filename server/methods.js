ba29d9cb7c765878a8b6e5b85a97f0b0ad47b620
Malek Hakim <hakimelek@gmail.com>
2016-12-14 10:21:08 -0600

    Fixes bugs

--- server/methods.js
+++ server/methods.js
@@ -3,14 +3,13 @@ Meteor.methods({
     // We check for the existance of an expression
     if (calculation.expression) {
       var result = eval(calculation.expression);
-      if (result)
-        return Calculations.insert({
-          expression: calculation.expression,
-          result: result,
-          createdAt: new Date(),
-          sessionId: calculation.sessionId,
-          sessionColor: calculation.sessionColor
-        });
-    }
+      return Calculations.insert({
+        expression: calculation.expression,
+        result: result,
+        createdAt: new Date(),
+        sessionId: calculation.sessionId,
+        sessionColor: calculation.sessionColor
+      });
+  }
   }
 });
